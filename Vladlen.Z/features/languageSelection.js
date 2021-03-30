f8f5e2b9dd02fc6c46c2f997b5edf67ab4a3d8a9
vladlen <uladlenzubau@yandex.ru>
2021-03-30 15:09:39 +0300

    Cypress tests:User anonymous

---
+++ Vladlen.Z/features/languageSelection.js
@@ -0,0 +1,35 @@
+describe('language selection', function() {
+    it('selects Russian restaurant page language', function() {
+        cy.viewport(1366,768)
+        cy.visit ('https://wolt.com/en/ltu/vilnius/restaurant/cili-pizza-big')
+        cy.wait(3000)
+        cy.url()
+          .should('include', 'restaurant/cili-pizza-big')
+        cy.get('.ConsentsBanner__buttons___1PPnk > :nth-child(2) > .Button__button___1o5LE').click()
+        cy.get('.LanguageSelector__TextButton-sc-1yfb4nd-2 > span').scrollIntoView()
+          .click()
+          cy.get('.Select__arrowDown___tdHIs').click()
+          cy.get('[id="react-select-2--option-4"]').click()
+          cy.get('[data-localization-key="menu.translate-show-original-prompt"]').scrollIntoView()
+            .should('include.text','from Lithuanian to Russian')
+    })
+
+    it('selects original restaurant page language', function() {
+        cy.viewport(1366,768)
+        cy.wait(3000)
+        cy.get('.LanguageSelector__TextButton-sc-1yfb4nd-2 > span').click()
+        cy.get('[data-localization-key="menu.translate-prompt"]').scrollIntoView()
+          .should('include.text', 'This menu is in Lithuanian')
+    })
+
+    it('selects English restaurant page language', function() {
+        cy.viewport(1366,768)
+        cy.wait(3000)
+        cy.get('.LanguageSelector__TextButton-sc-1yfb4nd-2 > span').scrollIntoView()
+          .click()
+        cy.get('.Select__arrowDown___tdHIs').click()
+        cy.get('[id="react-select-3--option-3"]').click()
+        cy.get('[data-localization-key="menu.translate-show-original-prompt"]').scrollIntoView()
+          .should('include.text','from Lithuanian to English')
+    })
+})
